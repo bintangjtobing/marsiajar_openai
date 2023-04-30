a7787b21cc764b0f56cdac0b20ec47577eb03225
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2023-04-30 23:29:07 +0700

    update: openai

---
+++ request.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Orhanerday\OpenAi\OpenAi;
+$open_ai = new OpenAi ('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');
+// get prompt parameter
+$prompt = $_GET['prompt'];
+// set api data
+$complete = $open_ai->completion([
+    'model' => 'text-davinci-003',
+    'prompt' => $prompt,
+    'temperature' => 1,
+    'max_tokens' => 4000,
+    'top_p' => 1,
+    'frequency_penalty' => 0,
+    'presence_penalty' => 0,
+    'stream' => true
+], function($curl_info, $data){
+    // now we will get stream data
+    echo $data;
+    echo PHP_EOL;
+    ob_flush();
+    flush();
+    return strlen($data);
+});
+?>
